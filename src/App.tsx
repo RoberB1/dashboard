211828a3727768ac7f0a32cee23a89efac12827d
RoberB1 <150208498+RoberB1@users.noreply.github.com>
2025-06-25 14:41:04 +0000

    desarrollo Guia 12

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Grid } from '@mui/material';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <div>
+            <h1>Bienvenido al Dashboard</h1>
+      </div>
+
+    
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+
+      <Grid container spacing={5} justifyContent="center" alignItems="center" >
+
+         {/* Encabezado */}
+         <Grid size={{ xs: 12, md: 12 }} >Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid >Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 9 }} >Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid
+         sx={{ display: { xs: "none", md: "block"} }} >
+         Elemento: Gráfico</Grid>
+
+         {/* Tabla */}
+         <Grid
+         sx={{ display: { xs: "none", md: "block" } }}>
+        Elemento: Tabla</Grid>
+
+         {/* Información adicional */}
+         <Grid>Elemento: Información adicional</Grid>
+
+      </Grid>
+
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
